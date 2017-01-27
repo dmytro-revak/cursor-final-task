f06c5a0754e6cde0bad8376b0d3b2e13b771787c
dmytro-revak <dmytro27@gmail.com>
2017-01-27 15:03:10 +0200

    created avatar and getIP functions

--- script/main.js
+++ script/main.js
@@ -1,19 +1,34 @@
 (function implemetGitHubPage() {
+
+  // Genetare and set avatar for personal IP
+  (function setAvatar() {
+    $('.avatar').attr('src', 'https://robohash.org/' + userIp + '.png');
+  })();
+
+  // Set user IP in name field in menu
+  (function setUserNameForMenu() {
+    $('.menu-user-name').text(userIp);
+  })();
+
   // Change logo when user hover it
-  $('.logo').hover(function() {
-      $(this).attr("src", "img/github-mark-blue.png");
-    }, function(){
-      $(this).attr("src", "img/github-mark.png");
-    }
-  );
+  (function changeLogo() {
+    $('.logo').hover(function() {
+        $(this).attr("src", "img/github-mark-blue.png");
+      }, function(){
+        $(this).attr("src", "img/github-mark.png");
+      }
+    );
+  })();
 
   // Redirect user on tru github search page after form submit 
-  $('.search-field').submit( function(event) {
-    event.preventDefault();
-    var searchInfo = $('.search-input').val();
-    searchInfo = searchInfo.replace(" ", "+");
-    window.location.replace("https://github.com/search?utf8=✓&q=" + searchInfo + "&type=Repositories&ref=searchresults");
-  });
+  (function implementSearching() {
+    $('.search-field').submit( function(event) {
+      event.preventDefault();
+      var searchInfo = $('.search-input').val();
+      window.location.href = ("https://github.com/search?utf8=✓&q=" + searchInfo + "&type=Repositories&ref=searchresults");
+    });
+  })();
+
 
 
 
